97bdc29fa8320a15723c112dd6c53a2d73f44158
niladhikari <niladhikari445@gmail.com>
2023-11-26 00:24:13 +0600

    add the google sign in system..there one in one user is using signup..another time they use same email...they only login

--- src/Pages/SocialLogin/SocialLogin.jsx
+++ src/Pages/SocialLogin/SocialLogin.jsx
@@ -1,11 +1,30 @@
-
 import { FaGoogle } from "react-icons/fa";
+import useAuth from "../../Hook/useAuth";
+import useAxiosPublic from "../../Hook/useAxiosPublic";
+import { useNavigate } from "react-router-dom";
 const SocialLogin = () => {
-    return (
-        <div>
+  const { SignInGoogle } = useAuth();
+  const axiosPublic = useAxiosPublic();
+  const navigate = useNavigate();
+
+  const handleGoogleSignIn = () => {
+    SignInGoogle().then((result) => {
+      console.log(result.user);
+      const userInfo = {
+        email: result.user?.email,
+        name: result.user?.displayName,
+      };
+      axiosPublic.post("/users", userInfo).then((res) => {
+        console.log(res.data);
+        navigate("/");
+      });
+    });
+  };
+  return (
+    <div>
       <div className="p-4 space-y-3 mb-7 text-center grid w-96 m-auto">
         <button
-          onClick={'handleGoogleSignIn'}
+          onClick={handleGoogleSignIn}
           className="btn btn-outline  text-blue-600 "
         >
           <FaGoogle></FaGoogle>
@@ -13,7 +32,7 @@ const SocialLogin = () => {
         </button>
       </div>
     </div>
-    );
+  );
 };
 
 export default SocialLogin;
